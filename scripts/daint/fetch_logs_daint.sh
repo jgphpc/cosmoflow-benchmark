b93a54af73cdb0b841176d0d60224da7ec33ee2d
Lukas Drescher <lukas.drescher@cscs.ch>
2020-08-05 19:26:40 +0200

    Data benchmark evaluation finished

--- scripts/daint/fetch_logs_daint.sh
+++ scripts/daint/fetch_logs_daint.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-# Parameters are derived from folder structure in results directory
-# $1 is the experiment type (weak_scaling, data_benchmark, etc.)
-# $2 is the timestamp_hostname label
+set -euo pipefail
 
-scp -r "daint101:/scratch/snx3000/lukasd/mlperf/cosmoflow-benchmark/logs/{$(logfiles=""; while read -r line; do logfiles+=,$line; done < <(ssh daint101 'cd /scratch/snx3000/lukasd/mlperf/cosmoflow-benchmark/logs && grep -l $2 *'); echo "${logfiles:1}")}" results/$1/$2/logs/
+# Parameters are derived from folder structure in results directory
+echo "$1 is the experiment type (weak_scaling, data_benchmark, etc.)"
+echo "$2 is the timestamp_hostname label"
+
+mkdir -p results/$1/$2
+scp -r "daint101:/scratch/snx3000/lukasd/mlperf/cosmoflow-benchmark/results/$1/$2" results/$1/
+
+mkdir -p results/$1/$2/logs/
+logfiles_list=$(logfiles=""; while read -r line; do logfiles+=,$line; done < <(ssh daint101 "cd /scratch/snx3000/lukasd/mlperf/cosmoflow-benchmark/logs && grep -l $2 *"); echo "${logfiles:1}")
+
+set -x
+scp -r "daint101:/scratch/snx3000/lukasd/mlperf/cosmoflow-benchmark/logs/{${logfiles_list}}" results/$1/$2/logs/
+set +x
